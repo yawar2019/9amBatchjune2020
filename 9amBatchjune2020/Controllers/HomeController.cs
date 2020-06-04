7b8f3d5711787a45102850f75b81795455745b16
yawar2019 <yawarali17@gmail.com>
2020-06-04 22:01:39 +0530

    PartialView Code committed

---
+++ 9amBatchjune2020/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _9amBatchjune2020.Models;
+namespace _9amBatchjune2020.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult getPartView()
+        {
+            return PartialView("_MyPartialView");
+        }
+
+        public ViewResult GetEmployee()
+        {
+            Employee obj = new Models.Employee();
+            obj.EmpId = 1;
+            obj.EmpName = "james";
+            obj.Empsalary = 45670;
+            return View(obj);
+        }
+    }
+}
---
+++ 9amBatchjune2020/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _9amBatchjune2020.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string  EmpName { get; set; }
+        public int Empsalary { get; set; }
+    }
+}
