429ebe4c9670b211e3b286bdf23e0089c9492e31
yawar2019 <yawarali17@gmail.com>
2020-06-19 08:56:30 +0530

    updated code

--- CodeFirstApproach/Controllers/DefaultController.cs
+++ CodeFirstApproach/Controllers/DefaultController.cs
@@ -15,5 +15,57 @@ namespace CodeFirstApproach.Controllers
         {
             return View(db.EmployeeModels.ToList());
         }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(FormCollection frm)
+        {
+            EmployeeModel obj = new EmployeeModel();
+            obj.EmpName=frm["EmpName"];
+            obj.EmpSalary = Convert.ToInt32(frm["EmpSalary"]);
+            
+            db.EmployeeModels.Add(obj);
+            int i = db.SaveChanges();
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+
+
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            EmployeeModel obj = db.EmployeeModels.Find(id);
+            return View(obj);
+        }
+        [HttpPost]
+        public ActionResult Edit(EmployeeModel obj)
+        {
+
+            db.Entry(obj).State = System.Data.Entity.EntityState.Modified;
+            
+            int i = db.SaveChanges();
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+
+
+        }
     }
 }
