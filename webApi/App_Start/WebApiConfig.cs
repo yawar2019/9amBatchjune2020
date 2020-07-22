a2920ab4629a3e968e8d1dfc12e420f807f5ff51
yawar2019 <yawarali17@gmail.com>
2020-07-22 06:52:02 +0530

    Updated wcf service

--- webApi/App_Start/WebApiConfig.cs
+++ webApi/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace webApi
 {
@@ -16,9 +17,10 @@ namespace webApi
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
-
+            var enableCorsAttribute = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(enableCorsAttribute);
             // Web API routes
-            config.MapHttpAttributeRoutes();
+            config.MapHttpAttributeRoutes();    
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
